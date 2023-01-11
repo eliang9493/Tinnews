9acfe91dbf48cdcad97ab0bd14959e0a1aee2a55
Emma Liang <97870806+eliang9493@users.noreply.github.com>
2023-01-10 23:29:39 -0600

    tinnews

---
+++ tinnews/ui/home/HomeViewModel.java
@@ -0,0 +1,34 @@
+package com.laioffer.tinnews.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+
+import com.laioffer.tinnews.model.NewsResponse;
+import com.laioffer.tinnews.repository.NewsRepository;
+
+public class HomeViewModel extends ViewModel {
+
+    private final NewsRepository repository;
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>();
+
+    // temp
+    public HomeViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    // event
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    // state
+    public LiveData<NewsResponse> getTopHeadlines() {
+        //observe countryInput
+        // switchMap: input => topHeadLines
+        // return topHeadLines
+        return Transformations.switchMap(countryInput, repository::getTopHeadlines);
+    }
+}
+
